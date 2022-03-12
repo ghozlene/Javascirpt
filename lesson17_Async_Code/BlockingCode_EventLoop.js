9e02f35346c1fc2acdb076f40aab9e51dbdbe702
Achref <medachrefg@gmail.com>
2022-03-12 21:55:23 +0100

    with async await (2/2)

--- lesson17_Async_Code/BlockingCode_EventLoop.js
+++ lesson17_Async_Code/BlockingCode_EventLoop.js
@@ -26,33 +26,33 @@ const setTimer = (duration) => {
 };
 
 async function trackUserHandler() {
-	// let positionData;
-	// let data;
-	// try {
-	// 	positionData = await ;
-	// 	data = await setTimer(3000);
-	// } catch (err) {
-	// 	console.log(err);
-	// }
-
+	let positionData;
+	let data;
+	try {
+		positionData = await getPosition();
+		data = await setTimer(3000);
+	} catch (err) {
+		console.log(err);
+	}
+	console.log(data, positionData);
 	/**
 	 * ! Chaining Promise with then can help you reading your data that not being
 	 * ! in promise at first but with Async await in the function assign with async
 	 * ! you need to wait until the resolve of the promise or the error and then
 	 * ! you will get the data after it
 	 */
-	getPosition()
-		.then((posData) => {
-			positionData = posData;
-			return setTimer(2000);
-		})
-		.catch((err) => {
-			console.log(err);
-			return 'on we go...';
-		})
-		.then((data) => {
-			console.log(data, positionData);
-		});
+	// getPosition()
+	// 	.then((posData) => {
+	// 		positionData = posData;
+	// 		return setTimer(2000);
+	// 	})
+	// 	.catch((err) => {
+	// 		console.log(err);
+	// 		return 'on we go...';
+	// 	})
+	// 	.then((data) => {
+	// 		console.log(data, positionData);
+	// 	});
 	setTimer(1000).then(() => {
 		console.log('time is up');
 	});
