84d97c1551ced3fc6bb55d733079dfe326bd41a6
Achref <medachrefg@gmail.com>
2022-03-13 12:39:27 +0100

    Promisifying HttpRequest using Promise/then

--- lesson18_Http_request/htttpRequest.js
+++ lesson18_Http_request/htttpRequest.js
@@ -2,18 +2,33 @@ const xhr = new XMLHttpRequest();
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
-xhr.responseType = 'json';
-xhr.onload = () => {
-	const listOfPosts = xhr.response;
-	for (const post of listOfPosts) {
-		const postEl = document.importNode(postTemplate.content, true);
+function sendHttpRequest(method, url) {
+	const promise = new Promise((resolve, reject) => {
+		xhr.open(method, url);
 
-		const title = postEl.querySelector('h2');
-		title.textContent = post.title;
-		const body = postEl.querySelector('p');
-		body.textContent = post.body;
-		listElement.append(postEl);
-	}
-};
-xhr.send();
+		xhr.responseType = 'json';
+		xhr.onload = () => {
+			resolve(xhr.response);
+		};
+		xhr.send();
+	});
+	return promise;
+}
+
+function fetchPost() {
+	sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts').then(
+		(responseData) => {
+			const listOfPosts = responseData;
+			for (const post of listOfPosts) {
+				const postEl = document.importNode(postTemplate.content, true);
+
+				postEl.querySelector('h2').textContent = post.title.toUpperCase();
+
+				postEl.querySelector('p').textContent = post.body;
+
+				listElement.append(postEl);
+			}
+		}
+	);
+}
+fetchPost();
