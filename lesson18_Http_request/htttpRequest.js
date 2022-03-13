eab1ee418392258113ccf293be029bb398241bbb
Achref <medachrefg@gmail.com>
2022-03-13 12:41:46 +0100

    Promisifying HttpRequest using async/await

--- lesson18_Http_request/htttpRequest.js
+++ lesson18_Http_request/htttpRequest.js
@@ -15,20 +15,22 @@ function sendHttpRequest(method, url) {
 	return promise;
 }
 
-function fetchPost() {
-	sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts').then(
-		(responseData) => {
-			const listOfPosts = responseData;
-			for (const post of listOfPosts) {
-				const postEl = document.importNode(postTemplate.content, true);
-
-				postEl.querySelector('h2').textContent = post.title.toUpperCase();
-
-				postEl.querySelector('p').textContent = post.body;
-
-				listElement.append(postEl);
-			}
-		}
+async function fetchPost() {
+	const responseData = await sendHttpRequest(
+		'GET',
+		'https://jsonplaceholder.typicode.com/posts'
 	);
+
+	const listOfPosts = responseData;
+	for (const post of listOfPosts) {
+		const postEl = document.importNode(postTemplate.content, true);
+
+		postEl.querySelector('h2').textContent = post.title.toUpperCase();
+
+		postEl.querySelector('p').textContent = post.body;
+
+		listElement.append(postEl);
+	}
 }
+
 fetchPost();
