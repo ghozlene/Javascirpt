728f844b7e59cdf6ea85d334ef5d0bc37e2c3366
Achref <medachrefg@gmail.com>
2022-03-13 12:06:58 +0100

    JSON data&parsing data

--- lesson18_Http_request/htttpRequest.js
+++ lesson18_Http_request/htttpRequest.js
@@ -4,5 +4,16 @@ const postTemplate = document.getElementById('single-post');
 
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
 xhr.responseType = 'json';
+xhr.onload = () => {
+	const listOfPosts = xhr.response;
+	for (const post of listOfPosts) {
+		const postEl = document.importNode(postTemplate.content, true);
 
+		const title = postEl.querySelector('h2');
+		title.textContent = post.title;
+		const body = postEl.querySelector('p');
+		body.textContent = post.body;
+		listElement.append(postEl);
+	}
+};
 xhr.send();
