4789ae82b2235a1dcc3b43d36642b7ce5288fca3
Achref <medachrefg@gmail.com>
2022-03-16 00:41:23 +0100

    working with cookies

---
+++ lesson22-localStorage/cookies.js
@@ -0,0 +1,9 @@
+const storeBtn = document.getElementById('store-btn');
+const retrieveBtn = document.getElementById('retrieve-btn');
+
+storeBtn.addEventListener('click', () => {
+	const userId = 'u123';
+	document.cookie = `uid=${userId}`;
+});
+
+retrieveBtn.addEventListener('click', () => {});
